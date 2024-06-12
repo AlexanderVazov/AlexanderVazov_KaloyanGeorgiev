41d459b33318dd3235621c02e7fd515df0b067f4
AlexanderVazov <alexander.vazov@gmail.com>
2024-06-13 00:57:40 +0300

    Made cosmetic changes to the code

--- server.js
+++ server.js
@@ -19,14 +19,9 @@ let OWinCount = 0;
 let tieCount = 0;
 let gameEnded = false;
 const winningCombinations = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8], // rows
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8], // columns
-  [0, 4, 8],
-  [2, 4, 6], // diagonals
+  [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
+  [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
+  [0, 4, 8], [2, 4, 6], // diagonals
 ];
 let turn = "X";
 global.obj;
